584a57a7069d14d5b712961519790da6444a3b43
ifat <i>
2024-08-09 22:11:46 +0100

    Handle form with onSubmit and access field data

--- src/assets/components/SimpleForm/SimpleForm.jsx
+++ src/assets/components/SimpleForm/SimpleForm.jsx
@@ -1,14 +1,24 @@
-
-
 const SimpleForm = () => {
-    return (
-        <div>
-            <form>
-                
-            </form>
-            
-        </div>
-    );
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.name.value);
+    console.log(e.target.email.value);
+    console.log(e.target.phone.value);
+    console.log("form submitted!");
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Name" name="name" />
+        <br />
+        <input type="email" placeholder="Email" name="email" id="" />
+        <br />
+        <input type="text" placeholder="Phone" name="phone" />
+        <br />
+        <input type="submit" value="Submit " />
+      </form>
+    </div>
+  );
 };
 
 export default SimpleForm;
